f40076b6c4da0cee2ffd6ad68cbfb4d66d0ab3bc
Cotyk <valentyn.vovchak@gmail.com>
2021-10-30 14:04:09 +0300

    Second Fix Commit

--- stiiizy/settings.py
+++ stiiizy/settings.py
@@ -83,9 +83,17 @@ WSGI_APPLICATION = 'stiiizy.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'stiiizy_db',
+        'USER': 'fish_user',
+        'PASSWORD': '131101',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
